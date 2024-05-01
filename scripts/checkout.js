fdf9c93d71fe70f0c1e448d7875e9274bace9620
Nvd-Kml <nivedkml@gmail.com>
2024-05-02 02:50:40 +0530

    Formatting and fix radio button issue

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 const getProduct = (cartItem) => {
     for (let product of products) {
@@ -12,8 +13,6 @@ const getProduct = (cartItem) => {
 let cartSummaryHtml = ``;
 
 cart.forEach((cartItem) => {
-
-    console.log(cartItem)
 
     const product = getProduct(cartItem);
 
@@ -31,7 +30,7 @@ cart.forEach((cartItem) => {
           ${product.name}
         </div>
         <div class="product-price">
-          ${(product.priceCents / 100).toFixed(2)}
+        ${formatCurrency(product.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -53,7 +52,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${product.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -66,7 +65,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${product.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -79,7 +78,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${product.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
